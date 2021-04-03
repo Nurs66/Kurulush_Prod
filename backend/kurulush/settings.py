6db744fbbed8d5c30706a2bf1b719f3dc94e8623
PythonNurs <nursultan.karagulov.dev@gmail>
2021-04-03 22:32:41 +0600

    fix redis server

--- backend/kurulush/settings.py
+++ backend/kurulush/settings.py
@@ -145,8 +145,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
