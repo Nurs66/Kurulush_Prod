23ded4b8568fae07eac01f54317da13b5e52b948
edzen12 <oichiev.edzen@gmail.com>
2021-07-31 19:27:36 +0600

    fix bug

--- backend/apps/form/tasks.py
+++ backend/apps/form/tasks.py
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-from django.conf import settings
-from django.core.mail import EmailMessage
-
-from kurulush.celery import app
-
-
-@app.task
-def send_info(
-        fullname: str, email: str,
-        number: str, comment: str
-        ):
-    message = f"""
-    ФИО: {fullname}
-    Телефон: {number}
-    Почта: {email}
-    Сообщение: {comment}
-    """
-    email = EmailMessage(
-        "Добро пожаловать в Kurulush Una!",
-        message, settings.EMAIL_HOST_USER,
-        ['oichiev.edzen@gmail.com', 'info@kyrgyzuna.kg'],
-    )
-    email.send(fail_silently=False)
-
-    # send_mail(
-    #     "Добро пожаловать в Kurulush Una!",
-    #     message, settings.EMAIL_HOST_USER,
-    #     ['oichiev.edzen@gmail.com', 'edznproger@gmail.com'],
-    #     fail_silently=False
-    # )
-=======
 from django.conf import settings
 from django.core.mail import EmailMessage
 
@@ -60,4 +28,3 @@ def send_info(
     #     ['oichiev.edzen@gmail.com', 'edznproger@gmail.com'],
     #     fail_silently=False
     # )
->>>>>>> 11d97f9093a3be1e57c024703a057dfad5c4720a
